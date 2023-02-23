45c1e4ee370d4a041ae79a70d6ff2c5abb9abe48
Semidime <semidime@protonmail.com>
2023-02-23 10:43:51 +0000

    removeFromArray updated to meet exercise criteria.
    removeFromArray():
    i) creates array containing items passed to function as first argument;
    ii) uses splice method to remove items from
    array that are included in other arguments passed to function;
    iii) loops to remove duplicate arguments;
    iv) returns array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray) {
+    
+    console.log(inputArray);
+    const outputArray = Array.from(arguments[0]);
+    console.log(outputArray);
+    console.log(arguments.length);
+    
+    for (let i = 1; i < arguments.length; i++) {
+        while (outputArray.includes(arguments[i])) {
+            outputArray.splice(outputArray.indexOf(arguments[i]),1);
+            console.log(outputArray);
+        }
+    }  
+
+    console.log(inputArray);
+    console.log(outputArray);
+
+    return outputArray
 
 };
 
