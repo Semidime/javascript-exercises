f035045910a2e2d17d86c31079eee5d1907faf22
Semidime <semidime@protonmail.com>
2023-02-23 16:57:20 +0000

    palindromes.js amended to meet exercise criteria

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (inputString) {
+
+    let inputLower = inputString.toLowerCase();
+    let stripPunctuation = inputLower.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    let stripSpaces = stripPunctuation.replace(/\s/g, "")
+    let reverse = stripSpaces.split("").reverse().join("");
+
+/* stringName.replace(/[^a-z]/g, "")  
+    CAN BE USED TO REPLACE EVERYTHING EXCEPT lc a-z */
+
+/*     console.log(inputLower);
+    console.log(stripPunctuation);
+    console.log(stripSpaces);
+    console.log(reverse);
+    console.log(stripSpaces === reverse); */
+
+    return (stripSpaces === reverse)
+
+    
 
 };
-
 // Do not edit below this line
 module.exports = palindromes;
