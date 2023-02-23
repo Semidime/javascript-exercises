aa2012aedf22fd268e7522ee84e83250cb631f4f
Semidime <semidime@protonmail.com>
2023-02-23 10:32:03 +0000

    reverseString.js updated meet exercise criteria.
    Function:
    i)  converts input string to an array of single chracters.
    ii) loops to pop characters out of array and
    concatenates to output string
    ii) returns output string.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    let revString = "";
+    const inputChars = [];
 
+    for (let i = 0; i < input.length; i++) {
+        inputChars.push(input.slice(i,i+1));
+    }
+
+    for (let n = 0; n < input.length; n++) {
+        revString += inputChars.pop();
+    }
+    return revString
 };
 
 // Do not edit below this line
