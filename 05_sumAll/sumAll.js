4aea774d73a21ae597fa31bcd52bb5d1cf265676
Semidime <semidime@protonmail.com>
2023-02-23 11:47:33 +0000

    sumAll function updated to meet test criteria.
    sumAll:
    i) accepts 2 parameters
    ii) returns an error if either argument is negative
    or if either argument is a non-number type
    iii) if both arguments are positive integers returns  the sum of those integers
    and all integers inbetween.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    
+    console.log(typeof(a));
+    console.log(typeof(b));
+  
+    let sum = 0;
 
+    if (a < 0 || b < 0 || typeof(a) !== "number" || typeof(b) !== "number") {
+        return "ERROR"
+    }
+    else if (a < b) {
+        for (i = a; i <= b; i++) {
+            sum += i;
+            console.log(sum);}
+    } else {
+        for (i = b; i <= a; i++) {
+            sum += i;
+            console.log(sum);}
+    }
+
+    return sum
 };
 
 // Do not edit below this line
