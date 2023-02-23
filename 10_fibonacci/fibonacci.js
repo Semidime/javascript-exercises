218da0003257c7d1f918f4c2de4c79b148f38789
Semidime <semidime@protonmail.com>
2023-02-23 18:20:07 +0000

    fibonacci function added.
    Function will accept 1 argument, and returns
     corresponding value from the Fibonacci sequence.
    
     function returns error if value added below zero.
    
     Otherwise creates an array containing n
     iterations of the Fibonacci sequence and returns elemnt n-1.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    if (n < 1) {
+        return "OOPS"
+    } else {
+        const fibArray = [1,1];
+
+        if (fibArray.length >= n) {
+            
+            console.log(fibArray[n-1]);
+            return fibArray[n-1];
+        } else {
+            
+            do {    
+            fibArray.push(fibArray[fibArray.length - 1] + fibArray[fibArray.length - 2]);
+            } while (fibArray.length < n);
+
+            console.log(fibArray);
+            console.log(fibArray[n-1]);
+            return fibArray[n-1];
+        }
+    }
 };
+
+fibonacci("12");
 
 // Do not edit below this line
 module.exports = fibonacci;
