fceb7bffce761a6b536ef8237fe0edeabf51ff84
Semidime <semidime@protonmail.com>
2023-02-24 11:33:10 +0000

    findTheOldest function added.
    Function
    - loops through an array of objects
    containing year of birth and year of death
    - caluclates current age, or age at death, for each object;
    - returns the object with the oldest age.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(inputArray) {
+    let evalAge = 0;
+    let oldestAge = 0;
+    let oldestPersonIndex = 0;
 
+    for (i=0; i < inputArray.length; i++) {
+
+        if (typeof inputArray[i].yearOfDeath === "undefined") {
+            const d = new Date();
+            evalAge = d.getFullYear() - inputArray[i].yearOfBirth
+
+        } else {
+            evalAge = inputArray[i].yearOfDeath - inputArray[i].yearOfBirth
+        }
+        
+        if (evalAge > oldestAge) {
+            oldestAge = evalAge;
+            oldestPersonIndex = i;
+        }
+    }
+
+    console.log(inputArray[oldestPersonIndex]);
+    return  inputArray[oldestPersonIndex];
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
