f6b94ed5ade8141b6dda84d42f5683a223e89255
Semidime <semidime@protonmail.com>
2023-02-24 13:17:15 +0000

    caesar function added.
    Function
    -accepts two arguments ("input string",n)
    -creates an array of single characters from the input string;
    -loops through the array to check if element is in range A-Z || a-z
    -if yes, shifts character by n chars, and adds to an output string
    -if no, adds element to output string unchanged
    -returns output string.

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,33 @@
-const caesar = function() {
+const caesar = function(inputString,n) {
+const inputArray = inputString.split("");
+console.log(inputArray);
+
+
+let outputString = "";
+upperCaseRef = [];
+lowerCaseRef = [];
+
+for (i=0; i < inputArray.length; i++) {
+    let letterRef = inputArray[i].charCodeAt()
+    
+    if (letterRef >= 65 && letterRef <= 90) {
+
+
+        outputString += String.fromCharCode(letterRef+n)
+    }
+    else if (letterRef >= 97 && letterRef <= 122) {
+        
+        outputString += String.fromCharCode(letterRef+n)
+    }
+    
+    else outputString += inputArray[i];
+}
+
+console.log(outputString);
+return outputString;
 
 };
+caesar('A', 1)
 
 // Do not edit below this line
 module.exports = caesar;
